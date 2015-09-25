616dba039168ac2eabbcdf2457e2e5ff24c13e95
mkabzan <kabzan@mangoweb.cz>
2015-09-25 21:19:54 +0200

    create repository

---
+++ Assets/feuerbeuch/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		if(isLocalPlayer)
+		{		
+			
+			GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled=true;
+			GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().enabled=true;
+		}
+	}
+}
+// Update is called once per frame
---
+++ Assets/feuerbeuch/Scripts/Unit_Spawn.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class Unit_Spawn: NetworkBehaviour {
+	public GameObject Player1_meleeIns;
+	public GameObject Player2_meleeIns;
+	public Vector2 player1SpawnPos;
+	public Vector2 player2SpawnPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			CmdPlayer1_meleeIns();
+			
+			
+		}
+
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			CmdPlayer2_meleeIns();
+			
+			
+		}
+
+	}
+	
+
+
+	[Command]
+	void CmdPlayer1_meleeIns()
+	{
+		GameObject tmp = Instantiate(Player1_meleeIns,player1SpawnPos,Quaternion.identity) as GameObject;
+		NetworkServer.Spawn(tmp);
+	}
+
+	[Command]
+	void CmdPlayer2_meleeIns()
+	{
+		GameObject tmp = Instantiate(Player2_meleeIns,player2SpawnPos,Quaternion.identity) as GameObject;
+		NetworkServer.Spawn(tmp);
+	}
+
+
+
+}
